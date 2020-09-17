92244f557c5f59ba3fdab548080dbdba012ec535
Alec Draymore <alec.draymore@gmail.com>
2020-09-17 16:10:18 -0400

    basic app layout

--- layouts/app-layout.js
+++ layouts/app-layout.js
@@ -2,14 +2,28 @@ import Head from 'next/head'
 
 export default function AppLayout({ children }) {
   return (
-    <div>
+    <>
       <Head>
         <title>TIO</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {children}
-      <footer>
-      </footer>
-    </div>
+      <div class="grid-y medium-grid-frame">
+        <div class="cell shrink top-bar">
+          <div class="top-bar-left">
+            <ul class="dropdown menu" data-dropdown-menu>
+              <li class="menu-text">Task It Out</li>
+            </ul>
+          </div>
+          <div class="top-bar-right">
+            <ul class="menu">
+            </ul>
+          </div>
+        </div>
+        {children}
+        <footer class="cell shrink">
+          <p>footer</p>
+        </footer>
+      </div>
+    </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import AppLayout from 'layouts/app-layout'
 export default function Home() {
   return (
     <AppLayout>
+      <div class="cell auto cell-block-container">
+        <h1>Page title</h1>
+        <p>page content</p>
+      </div>
     </AppLayout>
   )
 }
