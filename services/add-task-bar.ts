f330a8fd35471e88734db6b628cea8817780c5e3
Alec Draymore <Draymore26@gmail.com>
2020-09-20 15:24:36 -0400

    parse task string defaults due date to today if parsed due date is invalid

--- services/add-task-bar.ts
+++ services/add-task-bar.ts
@@ -2,12 +2,20 @@ import { TaskStatus } from 'types/task'
 import { v4 as uuid } from 'uuid'
 
 const parseTaskString = (str: string) => {
-  let name = (str.match(/.*?((?=[!@#$%^&*])|$)/) || [''])[0].trim()
-  let due = (str.match(/\$.*?(?=\s|$)/i) || [''])[0].trim().slice(1)
+  const parsedName = (str.match(/.*?((?=[!@#$%^&*])|$)/) || [''])[0].trim()
+  const parsedDue = (str.match(/\$.*?(?=\s|$)/i) || [''])[0].trim().slice(1)
+
+  const dueDate = new Date(parsedDue)
+
+  const name = parsedName
+  const due = dueDate.toString() === 'Invalid Date'
+    ? new Date()
+    : dueDate
+
   return {
       _key: uuid(),
       name,
-      due: new Date(due),
+      due,
       status: TaskStatus.open,
     }
 }
